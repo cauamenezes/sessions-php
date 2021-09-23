adf1d29b49b6bda656bf40e1d9443b25ad948acb
cauamenezes <cauamenezes.07@gmail.com>
2021-09-23 08:00:18 -0300

    erro de autenticação arrumado

--- processa_login.php
+++ processa_login.php
@@ -4,4 +4,4 @@ session_start();
 
 require("./funcoes.php");
 
-realizarLogin($_POST["txt_usuario"], $_POST["txt_senha"], lerArquivo("./usuarios.json"));
+realizarLogin($_POST["txt_usuario"], $_POST["txt_senha"], lerArquivo("./dados/usuarios.json"));
